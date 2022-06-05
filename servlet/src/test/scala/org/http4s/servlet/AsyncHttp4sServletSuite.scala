d961b47655bd61b7f2f7ad94b5b90dce2e836c44
Ross A. Baker <ross@rossabaker.com>
2022-06-05 01:31:11 -0400

    Better tests of non-blocking servlet IO

--- servlet/src/test/scala/org/http4s/servlet/AsyncHttp4sServletSuite.scala
+++ servlet/src/test/scala/org/http4s/servlet/AsyncHttp4sServletSuite.scala
@@ -17,20 +17,32 @@
 package org.http4s
 package servlet
 
+import cats.Monoid
 import cats.effect.IO
+import cats.effect.Deferred
 import cats.effect.Resource
 import cats.effect.std.Dispatcher
-import cats.syntax.all._
+import fs2.Chunk
+import fs2.Stream
 import munit.CatsEffectSuite
+import org.eclipse.jetty.client.HttpClient
+import org.eclipse.jetty.client.api.{Response => JResponse}
+import org.eclipse.jetty.client.util.BytesContentProvider
+import org.eclipse.jetty.client.util.DeferredContentProvider
 import org.http4s.dsl.io._
 import org.http4s.server.DefaultServiceErrorHandler
 import org.http4s.syntax.all._
 
-import java.net.URL
+import java.nio.ByteBuffer
 import scala.concurrent.duration._
-import scala.io.Source
 
 class AsyncHttp4sServletSuite extends CatsEffectSuite {
+  private val clientR = Resource.make(IO {
+    val client = new HttpClient()
+    client.start()
+    client
+  })(client => IO(client.stop()))
+
   private lazy val service = HttpRoutes
     .of[IO] {
       case GET -> Root / "simple" =>
@@ -47,63 +59,166 @@ class AsyncHttp4sServletSuite extends CatsEffectSuite {
   private val servletServer =
     ResourceFixture[Int](Dispatcher[IO].flatMap(d => TestEclipseServer(servlet(d))))
 
-  private def get(serverPort: Int, path: String): IO[String] =
-    Resource
-      .make(IO.blocking(Source.fromURL(new URL(s"http://127.0.0.1:$serverPort/$path"))))(source =>
-        IO.delay(source.close())
-      )
-      .use { source =>
-        IO.blocking(source.getLines().mkString)
-      }
+  private def get(client: HttpClient, serverPort: Int, path: String): IO[String] =
+    IO.blocking(
+      client.GET(s"http://127.0.0.1:$serverPort/$path")
+    ).map(_.getContentAsString)
 
   servletServer.test("AsyncHttp4sServlet handle GET requests") { server =>
-    get(server, "simple").assertEquals("simple")
+    clientR.use(get(_, server, "simple")).assertEquals("simple")
   }
 
-  servletServer.test("AsyncHttp4sServlet handle POST requests") { server =>
-    val contents = (1 to 14).map { i =>
-      val number =
-        scala.math.pow(2, i.toDouble).toInt - 1 // -1 for the end-of-line to make awk play nice
-      s"$i $number ${"*".*(number)}\n"
-    }.toList
+  // We should handle an empty body
+  servletServer.test("AsyncHttp4sServlet handle empty POST") { server =>
+    clientR
+      .use { client =>
+        IO.blocking(
+          client
+            .POST(s"http://127.0.0.1:$server/echo")
+            .send()
+        ).map(resp => Chunk.array(resp.getContent))
+      }
+      .assertEquals(Chunk.empty)
+  }
 
-    import org.asynchttpclient.Dsl._
-    import org.asynchttpclient.Response
+  // We should handle a regular, big body
+  servletServer.test("AsyncHttp4sServlet handle multiple chunks upfront") { server =>
+    val bytes = Stream.range(0, DefaultChunkSize * 2).map(_.toByte).to(Array)
+    clientR
+      .use { client =>
+        IO.blocking(
+          client
+            .POST(s"http://127.0.0.1:$server/echo")
+            .content(new BytesContentProvider(bytes))
+            .send()
+        ).map(resp => Chunk.array(resp.getContent))
+      }
+      .assertEquals(Chunk.array(bytes))
+  }
 
-    Resource.make(IO(asyncHttpClient()))(c => IO(c.close())).use { client =>
-      contents
-        .traverse { content =>
-          IO {
-            client
-              .preparePost(s"http://127.0.0.1:$server/echo")
-              .setBody(content)
-              .execute()
-              .toCompletableFuture()
-          }.flatMap { cf =>
-            IO.async[Response] { cb =>
-              IO.delay {
-                val stage = cf.handle[Unit] {
-                  case (response, null) => cb(Right(response))
-                  case (_, t) => cb(Left(t))
-                }
-                Some(IO.delay(stage.cancel(false)).void)
-              }
+  // We should be able to wake up if we're initially blocked
+  servletServer.test("AsyncHttp4sServlet handle single-chunk, deferred POST") { server =>
+    val bytes = Stream.range(0, DefaultChunkSize).map(_.toByte).to(Array)
+    clientR
+      .use { client =>
+        for {
+          content <- IO(new DeferredContentProvider())
+          bodyFiber <- IO
+            .async_[Chunk[Byte]] { cb =>
+              var body = Chunk.empty[Byte]
+              client
+                .POST(s"http://127.0.0.1:$server/echo")
+                .content(content)
+                .send(new JResponse.Listener {
+                  override def onContent(resp: JResponse, bb: ByteBuffer) = {
+                    val buf = new Array[Byte](bb.remaining())
+                    bb.get(buf)
+                    body ++= Chunk.array(buf)
+                  }
+                  override def onFailure(resp: JResponse, t: Throwable) =
+                    cb(Left(t))
+                  override def onSuccess(resp: JResponse) =
+                    cb(Right(body))
+                })
             }
-          }.flatMap { response =>
-            IO(response.getResponseBody())
-          }
+            .start
+          _ <- IO(content.offer(ByteBuffer.wrap(bytes)))
+          _ <- IO(content.close())
+          body <- bodyFiber.joinWithNever
+        } yield body
+      }
+      .assertEquals(Chunk.array(bytes))
+  }
+
+  // We should be able to wake up after being blocked
+  servletServer.test("AsyncHttp4sServlet handle two-chunk, deferred POST") { server =>
+    // Show that we can read, be blocked, and read again
+    val bytes = Stream.range(0, DefaultChunkSize).map(_.toByte).to(Array)
+    Dispatcher[IO]
+      .use { dispatcher =>
+        clientR.use { client =>
+          for {
+            content <- IO(new DeferredContentProvider())
+            firstChunkReceived <- Deferred[IO, Unit]
+            bodyFiber <- IO
+              .async_[Chunk[Byte]] { cb =>
+                var body = Chunk.empty[Byte]
+                client
+                  .POST(s"http://127.0.0.1:$server/echo")
+                  .content(content)
+                  .send(new JResponse.Listener {
+                    override def onContent(resp: JResponse, bb: ByteBuffer) =
+                      dispatcher.unsafeRunSync(for {
+                        _ <- firstChunkReceived.complete(()).attempt
+                        buf <- IO(new Array[Byte](bb.remaining()))
+                        _ <- IO(bb.get(buf))
+                        _ <- IO { body = body ++ Chunk.array(buf) }
+                      } yield ())
+                    override def onFailure(resp: JResponse, t: Throwable) =
+                      cb(Left(t))
+                    override def onSuccess(resp: JResponse) =
+                      cb(Right(body))
+                  })
+              }
+              .start
+            _ <- IO(content.offer(ByteBuffer.wrap(bytes)))
+            _ <- firstChunkReceived.get
+            _ <- IO(content.offer(ByteBuffer.wrap(bytes)))
+            _ <- IO(content.close())
+            body <- bodyFiber.joinWithNever
+          } yield body
         }
-        .assertEquals(contents)
-    }
+      }
+      .assertEquals(Monoid[Chunk[Byte]].combineN(Chunk.array(bytes), 2))
+  }
+
+  // We shouldn't block when we receive less than a chunk at a time
+  servletServer.test("AsyncHttp4sServlet handle two itsy-bitsy deferred chunk POST") { server =>
+    Dispatcher[IO]
+      .use { dispatcher =>
+        clientR.use { client =>
+          for {
+            content <- IO(new DeferredContentProvider())
+            firstChunkReceived <- Deferred[IO, Unit]
+            bodyFiber <- IO
+              .async_[Chunk[Byte]] { cb =>
+                var body = Chunk.empty[Byte]
+                client
+                  .POST(s"http://127.0.0.1:$server/echo")
+                  .content(content)
+                  .send(new JResponse.Listener {
+                    override def onContent(resp: JResponse, bb: ByteBuffer) =
+                      dispatcher.unsafeRunSync(for {
+                        _ <- firstChunkReceived.complete(()).attempt
+                        buf <- IO(new Array[Byte](bb.remaining()))
+                        _ <- IO(bb.get(buf))
+                        _ <- IO { body = body ++ Chunk.array(buf) }
+                      } yield ())
+                    override def onFailure(resp: JResponse, t: Throwable) =
+                      cb(Left(t))
+                    override def onSuccess(resp: JResponse) =
+                      cb(Right(body))
+                  })
+              }
+              .start
+            _ <- IO(content.offer(ByteBuffer.wrap(Array[Byte](0.toByte))))
+            _ <- firstChunkReceived.get
+            _ <- IO(content.offer(ByteBuffer.wrap(Array[Byte](1.toByte))))
+            _ <- IO(content.close())
+            body <- bodyFiber.joinWithNever
+          } yield body
+        }
+      }
+      .assertEquals(Chunk(0.toByte, 1.toByte))
   }
 
   servletServer.test("AsyncHttp4sServlet work for shifted IO") { server =>
-    get(server, "shifted").assertEquals("shifted")
+    clientR.use(get(_, server, "shifted")).assertEquals("shifted")
   }
 
   private def servlet(dispatcher: Dispatcher[IO]) = new AsyncHttp4sServlet[IO](
     service = service,
-    servletIo = NonBlockingServletIo[IO](4096),
+    servletIo = NonBlockingServletIo[IO](DefaultChunkSize),
     serviceErrorHandler = DefaultServiceErrorHandler[IO],
     dispatcher = dispatcher,
   )
