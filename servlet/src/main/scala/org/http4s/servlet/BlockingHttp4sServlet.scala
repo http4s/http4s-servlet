73df4cac055878a092eee72e1d0e7e166bcbc550
Ross A. Baker <ross@rossabaker.com>
2023-01-06 14:01:17 -0500

    Oh, it was imports.

--- servlet/src/main/scala/org/http4s/servlet/BlockingHttp4sServlet.scala
+++ servlet/src/main/scala/org/http4s/servlet/BlockingHttp4sServlet.scala
@@ -23,9 +23,9 @@ import cats.effect.std.Dispatcher
 import cats.syntax.all._
 import org.http4s.server._
 
-import scala.annotation.nowarn
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import scala.annotation.nowarn
 
 class BlockingHttp4sServlet[F[_]] private (
     service: HttpApp[F],
